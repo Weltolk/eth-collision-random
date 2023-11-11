b2766291ae6db2a28aed369ae262d92b50f82bae
Weltolk <40228052+Weltolk@users.noreply.github.com>
2023-11-11 15:56:01 +0800

    Update count.js

--- count.js
+++ count.js
@@ -11,6 +11,11 @@ if (!TELEGRAM_API_KEY || !TELEGRAM_CHAT_ID) {
 }
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage`;
 
+const FILE_PREFIX = "random";                                                                                                         
+const YES_FILENAME = `yes-${FILE_PREFIX}.txt`;                                                                                        
+const NO_FILENAME = `no-${FILE_PREFIX}.txt`;                                                                                          
+const ERR_FILENAME = `err-${FILE_PREFIX}.txt`;
+
 async function sendMessageViaTelegram(message) {
   try {
     await axios.post(TELEGRAM_API_URL, {
@@ -32,9 +37,9 @@ function readCountFromFile(filePath) {
 
 async function reportCounts() {
   try {
-    const noCount = readCountFromFile("no-count-random.txt");
-    const yesCount = await countLinesInFile("yes-random.txt");  // We still count lines for "yes" because it contains detailed records
-    const errCount = readCountFromFile("err-count-random.txt");
+    const noCount = readCountFromFile(NO_FILENAME);
+    const yesCount = await countLinesInFile(YES_FILENAME);  // We still count lines for "yes" because it contains detailed records
+    const errCount = readCountFromFile(ERR_FILENAME);
 
     const reportMessage = `${DEVICE_NAME}No: ${noCount}\nYes: ${yesCount}\nErr: ${errCount}`;
     await sendMessageViaTelegram(reportMessage);
